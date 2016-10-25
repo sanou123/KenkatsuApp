16bf154447817edb1354a53ffb0cbbf42983a795
daichi <hoge@email.com>
2016-10-25 17:40:27 +0900

    ポーズ機能搭載+USB通信機能(バグ有)

--- app/src/main/java/com/example/a1521315/test02/MenuSelect.java
+++ app/src/main/java/com/example/a1521315/test02/MenuSelect.java
@@ -16,8 +16,6 @@ public class MenuSelect extends Activity {
      * Called when the activity is first created.
      */
 
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
