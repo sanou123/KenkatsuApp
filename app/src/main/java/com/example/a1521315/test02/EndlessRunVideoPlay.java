707eb22ddcc26d504878a19dc6b41122f24f676d
pc55116 <sugawara622.17@gmail.com>
2016-12-15 10:33:12 +0900

    EndlessRunVideoPlayクラスを作った

---
+++ app/src/main/java/com/example/a1521315/test02/EndlessRunVideoPlay.java
@@ -0,0 +1,14 @@
+package com.example.a1521315.test02;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EndlessRunVideoPlay extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_endless_run_video_play);
+    }
+}
