5584614021cacc5a7cb4b5f392284c158c3253d3
seiya.s <hoge@email.com>
2017-02-03 09:21:21 +0900

    0203OK

--- app/src/main/java/com/example/a1521315/test02/MainActivity.java
+++ app/src/main/java/com/example/a1521315/test02/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.a1521315.test02;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends Activity {
@@ -19,10 +33,17 @@ public class MainActivity extends Activity {
     MediaPlayer mediaPlayer = null;
 
     private ProgressDialog progressDialog;
+    private Handler mHandler = new Handler();
+    private ScheduledExecutorService mScheduledExecutor;
+    private TextView mLblMeasuring;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         // タイトルバーを隠す
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         // ステータスバーを隠す
@@ -35,9 +56,8 @@ public class MainActivity extends Activity {
         title.setImageResource(R.drawable.kenkatsuapp_title);
         //playBGM();
 
-
-
-
+        //テキスト点滅表示
+        startMeasure();
     }
     //画面タップでユーザ選択に遷移
     @Override
@@ -77,6 +97,73 @@ public class MainActivity extends Activity {
         mediaPlayer.release();
         mediaPlayer = null;
     }
+
+    private void startMeasure() {
+
+
+        /**
+         * 点滅させたいView
+         * TextViewじゃなくてもよい。
+         */
+        mLblMeasuring = (TextView) findViewById(R.id.tap);
+
+        /**
+         * 第一引数: 繰り返し実行したい処理
+         * 第二引数: 指定時間後に第一引数の処理を開始
+         * 第三引数: 第一引数の処理完了後、指定時間後に再実行
+         * 第四引数: 第二、第三引数の単位
+         *
+         * new Runnable（無名オブジェクト）をすぐに（0秒後に）実行し、完了後1700ミリ秒ごとに繰り返す。
+         * （ただしアニメーションの完了からではない。Handler#postが即時実行だから？？）
+         */
+        mScheduledExecutor = Executors.newScheduledThreadPool(2);
+
+        mScheduledExecutor.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mLblMeasuring.setVisibility(View.VISIBLE);
+
+                        // HONEYCOMBより前のAndroid SDKがProperty Animation非対応のため
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                            animateAlpha();
+                        }
+                    }
+                });
+            }
+
+
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            private void animateAlpha() {
+
+                // 実行するAnimatorのリスト
+                List<Animator> animatorList = new ArrayList<Animator>();
+
+                // alpha値を0から1へ1000ミリ秒かけて変化させる。
+                ObjectAnimator animeFadeIn = ObjectAnimator.ofFloat(mLblMeasuring, "alpha", 0f, 1f);
+                animeFadeIn.setDuration(1000);
+
+                // alpha値を1から0へ600ミリ秒かけて変化させる。
+                ObjectAnimator animeFadeOut = ObjectAnimator.ofFloat(mLblMeasuring, "alpha", 1f, 0f);
+                animeFadeOut.setDuration(600);
+
+                // 実行対象Animatorリストに追加。
+                animatorList.add(animeFadeIn);
+                animatorList.add(animeFadeOut);
+
+                final AnimatorSet animatorSet = new AnimatorSet();
+
+                // リストの順番に実行
+                animatorSet.playSequentially(animatorList);
+
+                animatorSet.start();
+            }
+        }, 0, 1700, TimeUnit.MILLISECONDS);
+
+    }
+
 
 
     /**
